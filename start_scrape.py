83d83729a9a34ca7d14117684fe5e9f57e2ba325
matheusAparicio <matheusilva-m@outlook.com>
2022-01-07 16:30:37 -0300

    novas formas de se mandar email

--- start_scrape.py
+++ start_scrape.py
@@ -1,33 +1,37 @@
-from bs4 import BeautifulSoup
-from openpyxl import workbook
-from main import main
-from email.message import EmailMessage
-from segredo import EMAIL_ADRESS, EMAIL_PASSWORD
-import smtplib
-import os
 import re
+from email.message import EmailMessage
+import smtplib
+from openpyxl import Workbook
+from bs4 import BeautifulSoup
 
-book = workbook()
 enviar_email = True
+
+book = Workbook()
 sheet = book.active
 
-def start_scrape(page):     
+
+def start_scrape(subject:str, content:str, login:str, password='', emailsManual=[], page=''):
+    
     scrape = BeautifulSoup(page, 'html.parser')
     scrape = scrape.get_text()
+  
     emails = set(re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,3}", scrape))
     email_list = list(emails)
+    for item in emailsManual:
+        email_list.insert(0, item)
        
     if enviar_email:
       for row in zip(email_list):
-        sheet.append(row)
-        msg = EmailMessage()
-        msg['Subject'] = 'Olá'
-        msg.set_content('testando um bot de python com webscraping')
-        msg['From'] = EMAIL_ADRESS
-        msg['To'] = row[0]
-        print(row[0])
-        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-          smtp.login(EMAIL_ADRESS, EMAIL_PASSWORD)
-          smtp.auth_login()
-          smtp.send_message(msg)
+            
+            sheet.append(row)
+            msg = EmailMessage()
+            msg['Subject'] = subject
+            msg.set_content(content)
+            msg['From'] = login
+            msg['To'] = row[0]
+            print(row[0])
+            with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+              smtp.login(login, password)
+              smtp.auth_login()
+              smtp.send_message(msg)
       
